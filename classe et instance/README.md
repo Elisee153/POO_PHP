a8255773be36b9b54f34204895f8c263016bd749
jeanluc kabulu <jeanluc.kabulu@gmail.com>
2018-12-09 21:45:05 +0200

    Create classe and instance

---
+++ classe et instance/README.md
@@ -0,0 +1,11 @@
+# La Programmation Orientée en PHP :elephant:
+
+
+Les opérations de base pour l’utilisation des objets sont la création d’une classe et la défi-
+nition de ses **propriétés** et des **méthodes** qui vont permettre aux objets créés à partir de la
+classe d’agir sur leurs propriétés ou de communiquer avec leur environnement. Vient
+ensuite la création des objets proprement dits en tant qu’instances de la classe de base.
+
+# creation d'une classe
+
+pour creer une classe
---
+++ classe et instance/class_type.php
@@ -0,0 +1,21 @@
+<?php
+    class ma_class
+    {
+        //Définition d'une constante
+        const lang="PHP 5";
+
+        //Définition des variables de la classe
+        //on appel cela des attributs
+        public $prop1;
+        public $prop2 = "valeur";
+        public $prop3 = array("valeur0", "valeur1");
+
+        // on appel cela : la methode des classes 
+        public function ma_fonction($param1, $param2)
+        {
+            // corps de la fonction
+        }
+
+    }
+    // fin de la classe
+?>
